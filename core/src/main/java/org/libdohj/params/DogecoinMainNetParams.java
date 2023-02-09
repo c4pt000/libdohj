76e6903f29bd7d94ebbe3c94f37d8bebef606b0c
19YBeABhyk9tY5z5zGbA3oQLU5MFExpb8o <46433702+c4pt000@users.noreply.github.com>
2023-02-09 14:04:23 -0500

    Update DogecoinMainNetParams.java

--- core/src/main/java/org/libdohj/params/DogecoinMainNetParams.java
+++ core/src/main/java/org/libdohj/params/DogecoinMainNetParams.java
@@ -66,8 +66,8 @@ public class DogecoinMainNetParams extends AbstractDogecoinParams {
         bip32HeaderP2PKHpub = 0x02facafd; //The 4 byte header that serializes in base58 to "dgub".
         bip32HeaderP2PKHpriv =  0x02fac398; //The 4 byte header that serializes in base58 to "dgpv".
         genesisBlock.setDifficultyTarget(0x1e0ffff0L);
-        genesisBlock.setTime(1622171724L);
-        genesisBlock.setNonce(491158L);
+        genesisBlock.setTime(1675886721L);
+        genesisBlock.setNonce(1195632L);
         subsidyDecreaseBlockCount = 100000;
         spendableCoinbaseDepth = 10;
 
